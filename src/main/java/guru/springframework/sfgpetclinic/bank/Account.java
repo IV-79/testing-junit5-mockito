c39d7db0f25c5330c0e7139ffbc410083a25600a
Yves Pralong <pralong.yves@gmail.com>
2026-06-03 12:08:47 +0200

    Implemented Trasnsaction Added to deposit() the transaction Added to withdraw() the transaction

--- src/main/java/guru/springframework/sfgpetclinic/bank/Account.java
+++ src/main/java/guru/springframework/sfgpetclinic/bank/Account.java
@@ -1,10 +1,25 @@
 package guru.springframework.sfgpetclinic.bank;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Account {
     private int balance = 0;
 
+    private final List<Transaction> transactions =
+            new ArrayList<>();
+
+    public record Transaction(
+            LocalDate date,
+            int amount,
+            int balance
+    ) {
+    }
+
     public void deposit(int amount) {
         balance += amount;
+        transactions.add(new Transaction(LocalDate.now(), amount, balance));
     }
 
     public int getBalance() {
@@ -15,6 +30,12 @@ public class Account {
         if(balance < amount) {
             throw new IllegalArgumentException("Insufficient Balance");
         }
+
         balance -= amount;
+        transactions.add(new Transaction(LocalDate.now(), -amount, balance));
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
     }
 }
