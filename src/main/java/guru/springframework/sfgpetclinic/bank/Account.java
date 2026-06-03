54b2ce1bd43bdb3972bc2c574b15dfbe0100be9e
Yves Pralong <pralong.yves@gmail.com>
2026-06-03 10:02:39 +0200

    Creation of Account class with deposite() and getBalance() methode

---
+++ src/main/java/guru/springframework/sfgpetclinic/bank/Account.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.bank;
+
+public class Account {
+    private int balance = 0;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
